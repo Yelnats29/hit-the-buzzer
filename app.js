29106d37d2d5c6149437e32fd6e9f4924516ea9b
Yelnats <s.jeanjr@gmail.com>
2024-04-10 16:17:30 -0400

    created a timeout and clear message function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //First Assignment of elements
 const buzzerBtn = document.getElementById("buzzer")
 const messageElement = document.getElementById("message")
+const audio = document.getElementById("buttonSound")
 
 
     //Function to get the message to show
@@ -8,9 +9,19 @@ const messageElement = document.getElementById("message")
         messageElement.textContent = message
     }
 
+    //Function to clear message after button press
+    function clearMessage() {
+        setTimeout(function() {
+            messageDisplay("")}, 4000);
+    }
+
+
     //Function for Button Action
     function buzzerClicked() {
+        messageDisplay("");
         messageDisplay("Looks like someone just got a giant cookie!");
+        audio.play();
+        clearMessage();
     }
 
     //This makes the button activate
