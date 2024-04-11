2300f7c0e989ca0cea5494d02dc07cd406bfb8d6
Yelnats <s.jeanjr@gmail.com>
2024-04-11 12:56:05 -0400

    added audio file from repo

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-//First Assignment of elements
+//Assignment of references
 const buzzerBtn = document.getElementById("buzzer")
 const messageElement = document.getElementById("message")
-const audio = document.getElementById("buttonSound")
+const victory = document.getElementById("buttonSound")
 
 
-    //Function to get the message to show
-    function messageDisplay(message) {
-        messageElement.textContent = message
-    }
+//Function to get the message to show
+function messageDisplay(message) {
+    messageElement.textContent = message
+}
 
-    //Function to clear message after button press
-    function clearMessage() {
-        setTimeout(function() {
-            messageDisplay("")}, 4000);
-    }
+//Function to clear message after button press
+function clearMessage() {
+    setTimeout(function () {
+        messageDisplay("")
+    }, 6000);
+}
 
 
-    //Function for Button Action
-    function buzzerClicked() {
-        messageDisplay("");
-        messageDisplay("Looks like someone just got a giant cookie!");
-        audio.play();
-        clearMessage();
-    }
+//Function for Button Action
+function buzzerClicked() {
+    messageDisplay("");
+    messageDisplay("Looks like someone just got a giant cookie!");
+    victory.play();
+    clearMessage();
+}
 
-    //This makes the button activate
-    buzzerBtn.addEventListener('click', buzzerClicked);
+//This makes the button activate
+buzzerBtn.addEventListener('click', buzzerClicked);
