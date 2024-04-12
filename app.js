178a6e325aa7af3a7773788c70eb7d4b4d53c2b0
Yelnats <s.jeanjr@gmail.com>
2024-04-12 16:31:03 -0400

    Adding cookie images

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const buzzerBtn = document.getElementById("buzzer")
 const messageElement = document.getElementById("message")
 const victory = document.getElementById("buttonSound")
 
+//Function to have a random number of 1-3 be rolled
+function randomCookie() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
 
 //Function to get the message to show
 function messageDisplay(message) {
@@ -13,7 +18,7 @@ function messageDisplay(message) {
 function clearMessage() {
     setTimeout(function () {
         messageDisplay("")
-    }, 6000);
+    }, 8000);
 }
 
 
@@ -23,16 +28,38 @@ function buzzerClicked() {
     messageDisplay("Looks like someone just got a giant cookie! Congrats!");
     victory.play();
     clearMessage();
+
+    const cookie = randomCookie();
+
+    // Display the choices
+    const cookies = ['Sugar Cookie', 'Chocolate Chip Cookie', 'Oatmeal Cookie'];
+
+    setTimeout(() => {
+        if (cookie === 1) {
+            message.textContent = `You got a ${cookies[0]}! It's SUPER sweet!`;
+            message.className = 'cookie1';
+        } else if (cookie === 2) {
+            message.textContent = `You got a ${cookies[1]}! Nothing wrong with the classics!`;
+            message.className = 'cookie2';
+        } else {
+            message.textContent = `You got a ${cookies[2]}! A personal favorite.`;
+            message.className = 'coookie3';
+        }
+    }, 4000);
 }
 
-//Funtion to select the cookie of choice
+//This makes the button activate
+buzzerBtn.addEventListener('click', buzzerClicked);
 
-//Show this with a slow timer and then transition in the picture of the selected cookie.
-// My browser game has an example of this with Will and my burger menu has a code for slow transitions.
+
+
+
+
+
+
+
 
 // Lets also replace the button with the cookie after the selection??
 
 // Can I then replace the cookie with a bit version of it and say a message such as, "I hope you enjoyed that as much as I did" or "such a satisfying crunch" with a crunch audio behind it.
 
-//This makes the button activate
-buzzerBtn.addEventListener('click', buzzerClicked);
